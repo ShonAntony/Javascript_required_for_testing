066795226ef3fc17c797d2f75b3a046601d6eb7c
ShonAntony <shon.antony99@gmail.com>
2024-08-05 12:48:38 +0530

    Switch case has been added

--- condition_statements.js
+++ condition_statements.js
@@ -77,3 +77,39 @@ function testNum(num){
 }
 
 testNum(10);
+
+
+// switch case similar to how it works in java 
+
+function checkDay(dayNumb){
+    let day;
+    switch (dayNumb) {
+        case 1:
+            day="Monday";            
+            break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+        case 7:
+            day="Sunday";
+            break;
+        default:
+            day="no day is selected";
+        
+    }
+    console.log(day);    
+}
+
+checkDay(2);
