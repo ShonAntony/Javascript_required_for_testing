b96d81b0c1f97db37c7cd06836c3b73c03159f28
ShonAntony <shon.antony99@gmail.com>
2024-08-05 12:30:34 +0530

    conditional values

---
+++ condition_statements.js
@@ -0,0 +1,79 @@
+
+// the below function is to check the age of the voter and this is an example for the if() else statements 
+function checkAge(age){
+    if(age>=18){
+        console.log("it is the right age to vote");
+    }
+    else{
+        console.log("you are not elegible to vote");
+    }
+}
+
+checkAge(10);  // you are not elegible to vote
+checkAge(18);  // it is the right age to vote
+
+// if we need to check if the number is a positive number or a neg number we need to use more than one if conditions and the example for it is given below 
+
+function checkNumb(number){
+    if(number>0){
+        console.log("The number is a positive number");
+    }
+    if(number<0){
+        console.log("The number is a negative number");
+    }
+    if(number==0){
+        console.log("the number is zero");
+    }
+   
+}
+
+checkNumb(10); // The number is a positive number
+checkNumb(-10); // The number is a negative number
+checkNumb(0); // The number is a negative number
+
+//  if we need to assign the grades based on the markes which we have recieved we are going to use else-if
+
+
+function checkGrade(score){
+    let grade;
+    if(score>=90){
+//A   
+        grade="A";
+    }
+    else if(score>=80 && score<90){
+//B
+        grade="B";
+    }
+    else if(score>=50 && score<80){
+//C        
+        grade="C";
+    }
+    else if(score>=40 && score<50){
+//D   
+        grade="D";
+    }
+    else{
+//F
+        grade="F";
+    }
+    console.log(grade);
+}
+
+checkGrade(50);
+checkGrade(95);
+checkGrade(80);
+checkGrade(39);
+
+// below we are going to use a nested if else
+function testNum(num){
+    if(num>=10){
+        console.log("greater than or equal to 10");
+    }
+    else{
+        if(num<10){
+            console.log("lesser than 10");
+        }
+    }
+}
+
+testNum(10);
